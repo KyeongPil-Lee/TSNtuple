1f29f9050cdb63c34716f5fdf6e26b9b8c963632
Kyeongpil Lee <kplee@cern.ch>
2018-03-12 19:43:51 +0900

    define ntuple path inside the code

--- Analyzer/TagProbe/v04_Mu8Leg/MakeHist_TnP_Mu8Leg_Customized.cxx
+++ Analyzer/TagProbe/v04_Mu8Leg/MakeHist_TnP_Mu8Leg_Customized.cxx
@@ -1,10 +1,10 @@
 #include <TagProbe/v04_Mu8Leg/MakeHist_TnP_Mu8Leg.h>
 
-void MakeHist_TnP_Mu8Leg()
+void MakeHist_TnP_Mu8Leg_Customized()
 {
 	TString basePath = "/u/user/kplee/SE_UserHome/SingleMuon";
-	TString dataPath = basePath + "/crab_TSntuple_v20180311_DimuonLeg_SingleMuon_Run2017Fv1_Run305636_Default/180311_021428/0000/*.root";
-	TString outputFileName = "ROOTFile_Default.root";
+	TString dataPath = basePath + "/crab_TSntuple_v20180311_DimuonLeg_SingleMuon_Run2017Fv1_Run305636_Customized/180311_021441/0000/*.root";
+	TString outputFileName = "ROOTFile_Customized.root";
 
 	HistProducer* producer = new HistProducer();
 	producer->SetOutputFileName( outputFileName );
