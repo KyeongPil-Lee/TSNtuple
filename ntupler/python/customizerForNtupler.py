f3b80ea40c1fab8d7c240482b4a5c1e811920691
Kyeongpil Lee <kplee@cern.ch>
2018-05-05 19:04:43 +0200

    customizer for ntupler that can be added in HLT config.

---
+++ ntupler/python/customizerForNtupler.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+def customizerForNtuplerHLT(process, newProcessName = "MYHLT"):
+    if hasattr(process, "DQMOutput"):
+        del process.DQMOutput
+
+        flag_ReRun = True
+
+        from TSNtuple.ntupler.ntupler_cfi import ntuplerBase
+
+        process.ntupler = ntuplerBase.clone()
+        process.ntupler.OfflineMuon = cms.untracked.InputTag("muons")
+        process.ntupler.L3Muon = cms.untracked.InputTag("hltIterL3MuonCandidates")
+        process.ntupler.L2Muon = cms.untracked.InputTag("hltL2MuonCandidates")
+        process.ntupler.TriggerResults = cms.untracked.InputTag("TriggerResults", "", "HLT")
+        process.ntupler.TriggerEvent = cms.untracked.InputTag("hltTriggerSummaryAOD", "", "HLT")
+        process.ntupler.OfflineLumiScaler = cms.untracked.InputTag("scalersRawToDigi")
+
+        if flag_ReRun: # -- after HLT re-run -- #
+          process.ntupler.L1Muon           = cms.untracked.InputTag("hltGtStage2Digis", "Muon", newProcessName)
+          process.ntupler.MyTriggerResults = cms.untracked.InputTag("TriggerResults", "", newProcessName) # -- result after rerun HLT -- #
+          process.ntupler.MyTriggerEvent   = cms.untracked.InputTag("hltTriggerSummaryAOD", "", newProcessName) # -- result after rerun HLT -- #
+          process.ntupler.LumiScaler       = cms.untracked.InputTag("hltScalersRawToDigi", "", newProcessName)
+          process.ntupler.IterL3MuonNoID   = cms.untracked.InputTag("hltIterL3MuonsNoID", "", newProcessName)
+        else: # -- without HLT re-run -- #
+          process.ntupler.L1Muon = cms.untracked.InputTag("gmtStage2Digis", "Muon", "RECO")
+          process.ntupler.MyTriggerResults = cms.untracked.InputTag("TriggerResults")
+          process.ntupler.MyTriggerEvent = cms.untracked.InputTag("hltTriggerSummaryAOD")
+          process.ntupler.LumiScaler = cms.untracked.InputTag("hltScalersRawToDigi")
+
+        process.TFileService = cms.Service("TFileService",
+          fileName = cms.string("ntuple.root"),
+          closeFileFast = cms.untracked.bool(False),
+          )
+
+        process.mypath = cms.EndPath(process.ntupler)
+
+
