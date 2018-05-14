953e27f2fb190cd930fcf5364336447ace341b8a
Kyoengpil Lee <kplee@cern.ch>
2018-05-14 15:18:39 +0200

    change input

--- Analyzer/TagProbe/v06_Debug/v02_SampleTnPSelWithOld/MakeHist_default.cxx
+++ Analyzer/TagProbe/v06_Debug/v02_SampleTnPSelWithOld/MakeHist_default.cxx
@@ -8,10 +8,10 @@ void MakeHist_default() {
 
   histProducer->SetOutputFileName("ROOTFile_TnPHist_IsoMu27OverOffline_"+type+".root");
 
-  // vector<TString> rootFilePaths = Setting::rootFilePathsForType( type );
-  // for( const auto& rootFilePath : rootFilePaths )
-  //   histProducer->AddDataPath(rootFilePath);
-  histProducer->AddDataPath(Setting::rootFilePath_TEST);
+  vector<TString> rootFilePaths = Setting::rootFilePathsForType( type );
+  for( const auto& rootFilePath : rootFilePaths )
+    histProducer->AddDataPath(rootFilePath);
+  // histProducer->AddDataPath(Setting::rootFilePath_TEST);
   
   histProducer->Set_minPt( 29 ); // -- min pT applied for eta, phi and vtx
 
