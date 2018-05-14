7b67e9c2dc6b1fa141da89667619b3efd2aec1e0
Kyeongpil Lee <kplee@cern.ch>
2018-05-14 18:13:14 +0900

    debugging

--- Analyzer/TagProbe/Setting.h
+++ Analyzer/TagProbe/Setting.h
@@ -1,18 +1,23 @@
+#include <TString.h>
+
+#include <iostream>
+#include <vector>
+
 namespace Setting
 {
-  vector<TString> rootFilePaths_default = 
+  vector<TString> rootFilePaths_default = \
   {
-    "crab_TSNtuple_v20180513_3rdTry_OptimizeOI_Default/180513_164220/0000/ntuple*.root";
+    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180513_3rdTry_OptimizeOI_Default/180513_164220/0000/ntuple*.root"
   };
 
-  vector<TString> rootFilePaths_0p05 = 
+  vector<TString> rootFilePaths_0p05 = \
   {
-    "crab_TSNtuple_v20180513_3rdTry_OptimizeOI_0p05/180513_164232/0000/ntuple*.root";
+    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180513_3rdTry_OptimizeOI_0p05/180513_164232/0000/ntuple*.root"
   };
 
   vector<TString> rootFilePathsForType( TString type )
   {
-    vector<TString> rootFilePath_temp = 0;
+    vector<TString> rootFilePath_temp(0);
 
     if( type == "default" )
       return Setting::rootFilePaths_default;
@@ -21,7 +26,7 @@ namespace Setting
       return Setting::rootFilePaths_0p05;
     
     else
-      cout << "No corresponding rootFilePath! ... return NULL vector" << endl;
+      std::cout << "No corresponding rootFilePath! ... return NULL vector" << std::endl;
 
     return rootFilePath_temp;
   }
