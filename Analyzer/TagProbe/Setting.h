704b381e775e84eec91d3ec1a0675e22e07a5a5a
Kyeongpil Lee <kplee@cern.ch>
2018-05-15 19:24:16 +0900

    update with 2018 rerun

--- Analyzer/TagProbe/Setting.h
+++ Analyzer/TagProbe/Setting.h
@@ -7,12 +7,12 @@ namespace Setting
 {
   vector<TString> rootFilePaths_default = \
   {
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180513_3rdTry_OptimizeOI_Default/180513_164220/0000/ntuple*.root"
+    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180514_Run316110_2p0e34_OptimizeOI_Default/180514_202548/0000/ntuple*.root"
   };
 
   vector<TString> rootFilePaths_0p05 = \
   {
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180513_3rdTry_OptimizeOI_0p05/180513_164232/0000/ntuple*.root"
+    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180514_Run316110_2p0e34_OptimizeOI_0p05/180514_202602/0000/ntuple*.root"
   };
 
   TString rootFilePath_TEST = "/Users/KyeongPil_Lee/ServiceWorks/MuonHLT/v20180507_v01_UpdateTnPCode/TSNtuple/Analyzer/TagProbe/ExampleCodes/ntuple_9.root";
---
+++ Analyzer/TagProbe/v05_IsoMu27OverOffline_ProcessNameHLT/MakeHist.cxx
@@ -0,0 +1,19 @@
+#include <TagProbe/Setting.h>
+#include "MakeHist_IsoMu27OverOffline.h"
+
+void MakeHist() {
+  TString type = "default";
+
+  std::unique_ptr<HistProducer> histProducer( new HistProducer() );
+
+  histProducer->SetOutputFileName("ROOTFile_TnPHist_IsoMu27OverOffline_"+type+".root");
+
+  vector<TString> rootFilePaths = Setting::rootFilePathsForType( type );
+  for( const auto& rootFilePath : rootFilePaths )
+    histProducer->AddDataPath(rootFilePath);
+  // histProducer->AddDataPath(Setting::rootFilePath_TEST);
+  
+  histProducer->Set_minPt( 29 ); // -- min pT applied for eta, phi and vtx
+
+  histProducer->Produce();
+}
