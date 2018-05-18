5562fd02bb7f5d521bd632d2f85e9a9bf4041b3c
Kyoengpil Lee <kplee@cern.ch>
2018-05-19 00:24:23 +0200

    with KNU setup

--- Analyzer/TagProbe/v01_IsoMu27overL1/JobSubmitConfig.py
+++ Analyzer/TagProbe/v01_IsoMu27overL1/JobSubmitConfig.py
@@ -5,10 +5,26 @@ from TagProbe.JobSubmitTool import JobSubmitTool
 tool = JobSubmitTool()
 tool.outputPath = os.getcwd()
 tool.code = "MakeHist_IsoMu27overL1.cxx"
-tool.doCompile = False
-tool.scenario = "test"
+tool.doCompile = True
+
+tool.scenario = "default"
 tool.Submit()
 
-tool.scenario = "test2"
+tool.scenario = "90"
+tool.Submit()
+
+tool.scenario = "80"
+tool.Submit()
+
+tool.scenario = "70"
+tool.Submit()
+
+tool.scenario = "60"
+tool.Submit()
+
+tool.scenario = "50"
+tool.Submit()
+
+tool.scenario = "0p05"
 tool.Submit()
 
