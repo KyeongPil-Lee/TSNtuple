d5512b62a93db2899fea8b65cc815962514cf03b
Kyeongpil Lee <kplee@cern.ch>
2018-09-12 17:02:31 +0900

    fix typo

--- Analyzer/TagProbe/v01_OIoverL1/MakeHist_afterUpdate.cxx
+++ Analyzer/TagProbe/v01_OIoverL1/MakeHist_afterUpdate.cxx
@@ -5,7 +5,7 @@ void MakeHist_afterUpdate()
   std::unique_ptr<HistProducer> histProducer( new HistProducer() );
   histProducer->SetOutputFileName("ROOTFile_TnPHist_afterUpdate.root");
 
-  rootFilePath = "";
+  TString rootFilePath = "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180910_SingleMuon_Run2018D_Run322381_DCSOnly_Menuv3p5/180911_110639/0000/*.root";
   histProducer->AddDataPath( rootFilePath );
 
   histProducer->Set_minPt( 24 ); // -- min pT applied for eta, phi and vtx. 24: due to L1SingleMu22
--- Analyzer/TagProbe/v01_OIoverL1/MakeHist_beforeUpdate.cxx
+++ Analyzer/TagProbe/v01_OIoverL1/MakeHist_beforeUpdate.cxx
@@ -5,7 +5,7 @@ void MakeHist_beforeUpdate()
   std::unique_ptr<HistProducer> histProducer( new HistProducer() );
   histProducer->SetOutputFileName("ROOTFile_TnPHist_beforeUpdate.root");
 
-  rootFilePath = "";
+  TString rootFilePath = "/u/user/kplee/SE_UserHome/SingleMuon/crab_TSNtuple_v20180910_SingleMuon_Run2018D_Run322356_DCSOnly_Menuv3p4/180911_110630/0000/*.root";
   histProducer->AddDataPath( rootFilePath );
 
   histProducer->Set_minPt( 24 ); // -- min pT applied for eta, phi and vtx. 24: due to L1SingleMu22
